d87370129530fbbc83e0af9868891ba1503153ef
Mateo Courmont <courmontmateo@gmail.com>
2024-11-28 13:31:17 +0100

    commit

--- Formation_JS/TP4_B/fruitsTableau.js
+++ Formation_JS/TP4_B/fruitsTableau.js
@@ -1,45 +1,43 @@
 // Ajouter pour la touche "Entrée"
-document.getElementById('fruit').addEventListener('keydown', (event) => {
-    if (event.key === 'Enter') {
-      ajouterFruit();
-      event.preventDefault();
-    }
-  });
+document.getElementById("fruit").addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    ajouterFruit();
+    event.preventDefault();
+  }
+});
 
-document.getElementById('btnAjouter').onclick = ajouterFruit;
+document.getElementById("btnAjouter").onclick = ajouterFruit;
 
 const fruits = [];
-const item = document.getElementById("fruit")
-
+const item = document.getElementById("fruit");
 
 function ajouterFruit() {
-
-    const fruit = item.value
-    //Ajouter un fruit au tableau
-    fruits.push(fruit);
-    //Afficher le tableau en console
-    console.log(fruits);
-    // mettre un selecteur sur le template
-    const template = document.getElementById('ajouterFruit');
-    // cloner le template
-    const clone = template.content.cloneNode(true);
-    // Ajouter la ligne
-    clone.querySelector('td').innerHTML = fruit ;
-    // Ajouter un bouton "Supprimer"
-    const deleteButton = clone.querySelector('button');
-    deleteButton.onclick = () => {
+  const fruit = item.value;
+  //Ajouter un fruit au tableau
+  fruits.push(fruit);
+  //Afficher le tableau en console
+  console.log(fruits);
+  // mettre un selecteur sur le template
+  const template = document.getElementById("ajouterFruit");
+  // cloner le template
+  const clone = template.content.cloneNode(true);
+  // Ajouter la ligne
+  clone.querySelector("td").innerHTML = fruit;
+  // Ajouter un bouton "Supprimer"
+  const deleteButton = clone.querySelector("button");
+  deleteButton.onclick = () => {
     // Supprimer la ligne associée
-        deleteButton.closest('tr').remove();
+    deleteButton.closest("tr").remove();
 
-        const index = fruits.indexOf(fruit);
-        if (index > -1) {
-          fruits.splice(index, 1); // Retirer du tableau
-          console.log(fruits)
-        }
+    const index = fruits.indexOf(fruit);
+    if (index > -1) {
+      fruits.splice(index, 1); // Retirer du tableau
+      console.log(fruits);
     }
-// ajouter le template cloner dans le Tbody
-document.getElementById('myTbody').appendChild(clone); 
+  };
+  // ajouter le template cloner dans le Tbody
+  document.getElementById("myTbody").appendChild(clone);
 
-// Réinitialiser le champ de saisie
-document.getElementById('fruit').value = '';
+  // Réinitialiser le champ de saisie
+  document.getElementById("fruit").value = "";
 }
